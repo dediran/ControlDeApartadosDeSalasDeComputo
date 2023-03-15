91c9b874a47a3e5e9cc3e949d9245188ba6fe45f
JoseMixo <127899498+JoseMixo@users.noreply.github.com>
2023-03-14 18:12:21 -0600

    Add files via upload

--- Control De Apartados De Salas De Computo/CatalogoDeSalas.java
+++ Control De Apartados De Salas De Computo/CatalogoDeSalas.java
@@ -1,3 +1,58 @@
+import java.util.Scanner;
 public class CatalogoDeSalas {
-
+   
+   static Scanner leer = new Scanner(System.in);
+    
+    public void CatalogoDeSalas () {
+      
+      
+      /*do {
+         System.out.println("\nHOSPITAL PUERTA DE COBRE - CATALOGO DE PACIENTES");
+         System.out.println("[1] Alta de un paciente");
+         System.out.println("[2] Consulta de paciente");
+         System.out.println("[3] Consulta de todos los pacientes");
+         System.out.println("[4] Modificacion de un paciente");
+         System.out.println("[5] Baja de un paciente");
+         System.out.println("[6] Finalzar el programa");
+         System.out.print("Opcion deseada? : ");
+         opc = leer.nextInt();
+         
+         switch (opc) {
+         
+            case 1://alta de un paciente
+               realizarAlta(cp, cantidadPacientes);
+               break;
+               
+            case 2://consulta de un paciente
+            realizarConsultaIndividual(cp);
+            
+               break;   
+               
+            case 3://consulta de todos los pacientes
+            realizarConsultaGeneral(cp);
+               break;
+               
+           case 4://modificacion de un paciente
+           realizarModificacion(cp);
+            
+               break;
+               
+           case 5://baja de un paciente
+           realizarBaja(cp);
+            
+               break;
+               
+           case 6://Finalizar programa
+               System.out.print("Programa finalizado");
+               break;        
+           
+           default:
+               System.out.print("Opcion no valida...Presione ENTER para continuar");
+               limpiarPausar();  
+                
+         } 
+         
+      } while (opc != 6);*/
+    }
+    
 }
